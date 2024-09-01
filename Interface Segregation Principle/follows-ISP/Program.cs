491e2b6b2b31f73e0368c9a787d8d675b05f539d
moeinnajafi1998 <moeinnajafi1996@gmail.com>
2024-09-01 09:07:18 +0330

    Interface Segregation Principle

---
+++ Interface Segregation Principle/follows-ISP/Program.cs
@@ -0,0 +1,42 @@
+﻿// The Interface Segregation Principle (ISP) is another SOLID principle in object-oriented design. 
+// It states that a class should not be forced to implement interfaces it does not use. 
+// Instead of one large interface, multiple smaller, more specific interfaces should be created. 
+// This ensures that implementing classes only need to be concerned with the methods that are relevant to them
+public interface IWorkable
+{
+    void Work();
+}
+
+public interface IFeedable
+{
+    void Eat();
+}
+
+public class HumanWorker : IWorkable, IFeedable
+{
+    public void Work()
+    {
+        // Human worker implementation for work
+        Console.WriteLine("Human worker is working.");
+    }
+
+    public void Eat()
+    {
+        // Human worker implementation for eating
+        Console.WriteLine("Human worker is eating.");
+    }
+}
+
+public class RobotWorker : IWorkable
+{
+    public void Work()
+    {
+        // Robot worker implementation for work
+        Console.WriteLine("Robot worker is working.");
+    }
+}
+
+// Explanation:
+// Here, the IWorker interface is split into two smaller, more specific interfaces: IWorkable and IFeedable.
+// The HumanWorker class implements both IWorkable and IFeedable because it can work and eat.
+// The RobotWorker class only implements IWorkable since it only needs to work and doesn’t need to worry about the Eat method.
---
+++ Interface Segregation Principle/not-follow-ISP/Program.cs
@@ -0,0 +1,40 @@
+﻿public interface IWorker
+{
+    void Work();
+    void Eat();
+}
+
+public class HumanWorker : IWorker
+{
+    public void Work()
+    {
+        // Human worker implementation for work
+        Console.WriteLine("Human worker is working.");
+    }
+
+    public void Eat()
+    {
+        // Human worker implementation for eating
+        Console.WriteLine("Human worker is eating.");
+    }
+}
+
+public class RobotWorker : IWorker
+{
+    public void Work()
+    {
+        // Robot worker implementation for work
+        Console.WriteLine("Robot worker is working.");
+    }
+
+    public void Eat()
+    {
+        // RobotWorker does not eat, but is forced to implement the Eat method
+        throw new NotImplementedException();
+    }
+}
+
+// Explanation:
+// The IWorker interface has two methods: Work and Eat.
+// The HumanWorker class can implement both methods, but the RobotWorker class can only implement Work. Since robots don't eat, RobotWorker has to either throw an exception or provide a meaningless implementation for Eat.
+// This design violates the Interface Segregation Principle because RobotWorker is forced to implement an interface method (Eat) that it doesn't need.
